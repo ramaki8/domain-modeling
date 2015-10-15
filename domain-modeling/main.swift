89010767032a37f07f1e666055aba8c112d8dfbe
Rachel Kipps <rachelkipps@gmail.com>
2015-10-14 22:45:14 -0700

    Initial Commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Rachel Kipps on 10/14/15.
+//  Copyright © 2015 Rachel Kipps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
